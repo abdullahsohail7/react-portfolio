51bbc2ce5d8c6394e8a76b7a92f877c6e7ef2128
Syed Abdullah Abbas Sohail <abdullahsohail7@hotmail.com>
2022-06-14 23:39:29 +0500

    First Commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div name= "About" id='about'
-    className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+    className='w-full h-full-extra bg-gradient-to-b from-gray-800 to-black text-white'>
         <div className='max-w-screen-md p-4 mx-auto flex flex-row justify-center items-center w-fit h-fit'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
@@ -14,21 +14,21 @@ About                </p>
 CEO at Square Foot Industries            </p>
 </div>
         
-            <div className='py-4'>
-            <p className='text-l py-1'>
+            <div className='py-1'>
+            <p className='text-md py-1'>
 • Launched an FMCG Startup in 2018.</p> 
-<p className='text-l py-1'>• Leveraged planning knowledge to drive supply chain activities through the creation, modification and implementation of
+<p className='text-md py-1'>• Leveraged planning knowledge to drive supply chain activities through the creation, modification and implementation of
 efficient production plans. Efficient assembly systems were installed to ensure 15% more cost effective production process.</p>
-<p className='text-l py-1'>• Was able to create a strong Sales Network spanning 45 different cities.</p> 
-<p className='text-l py-1'>• In charge of hiring work force, managing operations on the floor, strategizing on every turn for market entrants.</p> 
-<p className='text-l py-1'>• Extensive Experience in Logistics and Warehouse Management.</p> 
-<p className='text-l py-1'>• Integrated ERP procedures to ensure smooth sales and purchase reports.</p> 
-<p className='text-l py-1'>• Implemented weekly sales analysis system to understand customer’s seasonal needs and deliver products accordingly.</p> 
-<p className='text-l py-1'>• Great experience in liaising with clients, suppliers and transport companies.</p> 
-<p className='text-l py-1'>• Monitored and communicated about the performance through KPIs Set dealer production allocation plans and stock</p>
+<p className='text-md py-1'>• Was able to create a strong Sales Network spanning 45 different cities.</p> 
+<p className='text-md py-1'>• In charge of hiring work force, managing operations on the floor, strategizing on every turn for market entrants.</p> 
+<p className='text-md py-1'>• Extensive Experience in Logistics and Warehouse Management.</p> 
+<p className='text-md py-1'>• Integrated ERP procedures to ensure smooth sales and purchase reports.</p> 
+<p className='text-md py-1'>• Implemented weekly sales analysis system to understand customer’s seasonal needs and deliver products accordingly.</p> 
+<p className='text-md py-1'>• Great experience in liaising with clients, suppliers and transport companies.</p> 
+<p className='text-md py-1'>• Monitored and communicated about the performance through KPIs Set dealer production allocation plans and stock</p>
 planning. 
-<p className='text-l py-1'>• Resolution of Dealer Issues with stock, customer orders.</p> 
-<p className='text-l py-1'>• Weekly production management/ coordination with principle for production requests.</p>            
+<p className='text-md py-1'>• Resolution of Dealer Issues with stock, customer orders.</p> 
+<p className='text-md py-1'>• Weekly production management/ coordination with principle for production requests.</p>            
        </div>
        </div>
         </div>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,7 +5,7 @@ import reactWeather from '../assets/portfolio/reactWeather.jpg'
 const Portfolio = () => {
   return (
     <div name="Portfolio" id="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full  text-white md:h-screen'>
-        <div className='max-w-screen-md p-4 mx-auto flex flex-col justify-center  w-full h-full'>
+        <div className='max-w-screen-md p-4 mx-auto flex flex-col justify-center  w-full h-full-extra'>
             <div className='pb-8 py-4'>
                 <p className='text-4xl  font-bold inline-table border-b-4 border-red-500'>Portfolio</p>
                 <p className='py-6'>My Projects</p>
